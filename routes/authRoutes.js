682bdcfc9c6bf98a78a11a1438c9fa07732b9196
Code-Gale <codewhiz001@gmail.com>
2024-05-15 17:03:28 +0100

    auth, order, product routes

---
+++ routes/authRoutes.js
@@ -0,0 +1,8 @@
+const router = require('express').Router()
+const { signup, login, logout } = require('../controllers/authController')
+
+router.post('/signup', signup)
+router.post('/login', login)
+router.post('/logout', logout)
+
+module.exports = router
